fbfc637198f03a767600e769f412487c20c2829e
vadim <osyukov.vadim.gmail.com>
2022-04-20 00:02:45 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
       .setTitle('Simple API')
       .setDescription('API description')
       .setVersion('1.0')
-      // .addTag('cats')
       .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('doc', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
