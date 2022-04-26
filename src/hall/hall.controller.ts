63856145cb51e758a1e5453dfd6e3c65cd4a64c0
vadim <osyukov.vadim.gmail.com>
2022-04-26 21:53:29 +0300

    fix

--- src/hall/hall.controller.ts
+++ src/hall/hall.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Post} from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
 import {AddHallDto} from "./dto/add-hall.dto";
 import {HallService} from "./services/hall.service";
 
@@ -7,7 +7,7 @@ export class HallController {
     constructor(private readonly hallService: HallService) {
     }
     @Post('addHall')
-    public async addHall(dto: AddHallDto): Promise<string>{
+    public async addHall(@Body() dto: AddHallDto): Promise<string>{
         const {name, photoIds, description} = dto
         const hall = await this.hallService.addHall({name, photoIds, description})
         return hall.id
