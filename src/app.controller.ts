5c9a1ed96980a08f0e63b7ef9f21c666a7c37797
vadim <osyukov.vadim.gmail.com>
2022-04-27 00:47:54 +0300

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import {Controller, Get} from "@nestjs/common";
 
 @Controller('/')
 export class AppController {
-    @Get('check')
+    @Get('health')
     public check(){
         return 'ok'
     }
