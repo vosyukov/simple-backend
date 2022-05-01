4bdf24d0ffb981199a097bd8d9639f596d316d78
vadim <osyukov.vadim.gmail.com>
2022-05-01 23:52:41 +0300

    fix

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller } from "@nestjs/common";
 import { CityService } from "./services/city.service";
 import { AddCityRequestDto } from "./dto/add-city.request.dto";
 import { AddCityResponseDto } from "./dto/add-city.response.dto";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("city")
 @Controller("city")
 export class CityController {
   constructor(private readonly cityService: CityService) {}
